f435865a2889f078f2c59fabb8b1bf4de04d0a34
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-06-08 13:58:45 -0500

    implemented with list

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,29 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+from singly_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        try:
+            self.storage.append(value)
+        finally:
+            self.size = self.size + 1
 
     def pop(self):
-        pass
+        try:
+            value = self.storage.pop()
+        except IndexError:
+            value = None
+        else:
+            self.size = self.size - 1
+
+        return value
