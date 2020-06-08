f976c948f3105277489308c9249cd24e8c9512c5
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-06-08 14:11:58 -0500

    implemented with LinkedList

--- queue/queue.py
+++ queue/queue.py
@@ -13,26 +13,31 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+from singly_linked_list import LinkedList
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
     
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
         try:
-            self.storage.append(value)
+            self.storage.add_to_tail(value)
         finally:
             self.size = self.size + 1
 
     def dequeue(self):
         try:
-            value = self.storage.pop(0)
+            value = self.storage.remove_head()
         except:
             value = None
         else:
-            self.size = self.size - 1
+            if self.size > 0:
+                self.size = self.size - 1
         
         return value
